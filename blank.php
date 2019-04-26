9874d7e21e2552ff0d7ffb99d41a81413757e7a8
Arthur Flávio de Abreu Tavares <277093@utramigbh.sede>
2019-04-26 20:34:34 -0300

    Adicionando includes e organizando documentos

---
+++ blank.php
@@ -0,0 +1,72 @@
+<?php
+// Headers
+include_once('includes/headers/header-init.php');
+include_once('includes/headers/header-styles.php');
+// Navbar
+include_once('includes/navbar/navbar-main.php');
+?>
+
+<div id="wrapper">
+    <!-- Sidebar -->
+    <ul class="sidebar navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-fw fa-folder"></i>
+                <span>Pages</span>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                <h6 class="dropdown-header">Login Screens:</h6>
+                <a class="dropdown-item" href="login.php">Login</a>
+                <a class="dropdown-item" href="register.php">Register</a>
+                <a class="dropdown-item" href="forgot-password.php">Forgot Password</a>
+                <div class="dropdown-divider"></div>
+                <h6 class="dropdown-header">Other Pages:</h6>
+                <a class="dropdown-item" href="404.php">404 Page</a>
+                <a class="dropdown-item active" href="blank.php">Blank Page</a>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="charts.php">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Charts</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="tables.php">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Tables</span></a>
+        </li>
+    </ul>
+
+    <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Breadcrumbs-->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="index.php">Dashboard</a>
+                </li>
+                <li class="breadcrumb-item active">Blank Page</li>
+            </ol>
+
+            <!-- Page Content -->
+            <h1>Blank Page</h1>
+            <hr>
+            <p>This is a great starting point for new custom pages.</p>
+
+        </div>
+        <!-- /.container-fluid -->
+
+        <?php
+        include('includes/footers/footer-init.php');
+        include('includes/footers/footer-modal.php');
+        include('includes/footers/footer-scripts.php');
+        include('includes/footers/footer-final.php');
+        ?>
---
+++ includes/headers/header-styles.php
@@ -0,0 +1,12 @@
+    <!-- Arquivos Imutaveis -->
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+    <!-- Page level plugin CSS-->
+    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet" />
+
+    <!-- Fim dos Arquivos Imutaveis -->
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="css/style.css">
