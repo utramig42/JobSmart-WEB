743b69d97a419ff255ab4de83d826c71f2ca6073
Arthur Tavares <arthurabreu00@gmail.com>
2019-05-21 21:47:34 -0300

    Criação da tela de cadastro de fornecedores

---
+++ cadastroFornecedores.php
@@ -0,0 +1,164 @@
+<?php
+    include_once('includes/headers/header-init.php');
+?>
+
+<title>Job'Smart - Cadastro de Fornecedor</title>
+
+<?php
+    include_once('includes/headers/header-styles.php');
+?>
+
+<link rel="stylesheet" href="css/forms.css">
+
+<?php
+    include_once('includes/navbar/navbar-main.php');
+?>
+
+<div id="wrapper">
+    <?php
+        include_once('includes/navbar/navbar-sidebar.php')
+    ?>
+
+    <div id="content-wrapper">
+        <div class="container-fluid">
+
+            <!-- Breadcrumbs-->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="index.php">Painel de controle</a>
+                </li>
+
+                <li class="breadcrumb-item">
+                    <a href="fornecedores.php">Fornecedores</a>
+                </li>
+
+                <li class="breadcrumb-item active">
+                    Cadastro de fornecedor
+                    <?php $fileName = 'Fornecedores'; ?>
+                </li>
+            </ol>
+
+            <div class="card mx-auto">
+                <div class="card-header">Dados do fornecedor</div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <div class="form-row">
+                                <div class="col-md-8">
+                                    <div class="form-label-group">
+                                        <input type="text" id="razao-social" name="razao-social" class="form-control" placeholder="Razão Social" required>
+                                        <label for="razao-social">Razão Social</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="form-label-group">
+                                        <input type="text" id="cnpj" name="cnpj" class="form-control" placeholder="CNPJ" required>
+                                        <label for="cnpj">CNPJ</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="form-row">
+                                <div class="col-md-8">
+                                    <div class="form-label-group">
+                                        <input type="text" id="nome-fantasia" name="nome-fantasia" class="form-control" placeholder="Nome Fantasia" required>
+                                        <label for="nome-fantasia">Nome Fantasia</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-1">
+                                    <div class="form-label-group">
+                                        <select id="uf" name="uf" class="form-control" required>
+                                            <option value="" selected>UF</option>
+                                        </select>
+                                        <label for="uf" class="d-none">UF</label>
+                                    </div>
+                                </div>
+
+                                <div class=" col-md-3">
+                                    <div class="form-label-group">
+                                        <select id="cidade" name="cidade" class="form-control" required>
+                                            <option value="" selected>Cidade</option>
+                                        </select>
+                                        <label for="cidade" class="d-none">Cidade</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="form-row">
+                                <div class="col-md-8">
+                                    <div class="form-label-group">
+                                        <input type="text" id="logradouro" name="logradouro" class="form-control" placeholder="Logradouro" required>
+                                        <label for="logradouro">Logradouro</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-2">
+                                    <div class="form-label-group">
+                                        <input type="number" id="numero" name="numero" class="form-control" placeholder="Número" required>
+                                        <label for="numero">Número</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-2">
+                                    <div class="form-label-group">
+                                        <input type="text" id="complemento" name="complemento" class="form-control" placeholder="Complemento" required>
+                                        <label for="complemento">Complemento</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="form-row">
+                                <div class="col-md-4">
+                                    <div class="form-label-group">
+                                        <input type="text" id="nome-contato" name="nome-contato" class="form-control" placeholder="Nome do Contato" required>
+                                        <label for="nome-contato">Nome do Contato</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="form-label-group">
+                                        <input type="text" id="e-mail" name="e-mail" class="form-control" placeholder="E-mail" required>
+                                        <label for="e-mail">E-mail</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="form-label-group">
+                                        <input type="text" id="telefone" name="telefone" class="form-control" placeholder="Telefone" required>
+                                        <label for="telefone">Telefone</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary" type="submit">Cadastrar</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /.content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php
+    include('includes/footers/footer-init.php');
+    include('includes/footers/footer-modal.php');
+    include('includes/footers/footer-scripts.php');
+?>
+
+<script src="./js/validantion"></script>
+
+<?php
+    include('includes/footers/footer-final.php');
+?>
