f220bbcf814724092b97af28db58bd388c727c31
Arthur Tavares <arthur@bertholdo.com.br>
2019-05-23 16:57:25 -0300

    Começando a parte de DAO

--- php-action/dll/UsuarioController.php
+++ php-action/dll/UsuarioController.php
@@ -1,4 +1,24 @@
 <?php
 
+// require_once '../dao/Sql.php';
+
 class UsuarioController
-{ }
+{ 
+
+  
+
+    public function __construct(){
+        echo("Oi");
+
+
+
+        $conn = mysqli_connect('localhost','root','','v1');
+        if(mysqli_errno($conn)){
+            echo 'Ok';
+        }else{
+            echo mysqli_errno($conn);
+        }
+        // $this ->Sql = new Sql();
+    }
+
+}
