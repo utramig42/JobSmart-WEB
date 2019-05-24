c7a10dcc56646bf820bcfcdf7c8389f7449fd9de
Arthur Tavares <arthurabreu00@gmail.com>
2019-05-23 21:39:42 -0300

    Isso ta muito bugado desculpa

--- php-action/dll/UsuarioController.php
+++ php-action/dll/UsuarioController.php
@@ -1,24 +1,47 @@
 <?php
 
-// require_once '../dao/Sql.php';
+require_once 'config.php';
 
 class UsuarioController
-{ 
-
-  
-
-    public function __construct(){
-        echo("Oi");
+{
 
 
 
-        $conn = mysqli_connect('localhost','root','','v1');
-        if(mysqli_errno($conn)){
-            echo 'Ok';
-        }else{
-            echo mysqli_errno($conn);
-        }
-        // $this ->Sql = new Sql();
+    public function __construct()
+    {
+
+        $this->listUsers();
     }
 
+
+    public function listUsers()
+    {
+        $command = "SELECT 
+        f.mat_fun AS 'matricula',
+        c.nm_cargo AS 'cargo',
+        f.nm_fun AS 'nome',
+        f.end_fun AS 'endereco',
+        f.uf_fun AS 'uf',
+        f.cid_fun AS 'cidade',
+        f.sal_fun AS 'salario',
+        f.cpf_fun AS 'cpf',
+        f.tel_fun AS 'telefone',
+        f.dt_nasc_fun AS 'data_nasc',
+        f.dt_res_fun AS 'data_recisao',
+        f.temp_ativo_fun AS 'temporario',
+        f.dt_admin AS 'data_admissao'
+    FROM
+        funcionário f
+            INNER JOIN
+        cargo c ON c.id_cargo = f.id_cargo WHERE c.id_cargo = ?";
+
+        $sql = new Connection();
+        $row = $sql->select($command);
+
+
+        while ($row) {
+            echo $row['matricula'];
+        }
+    }
 }
+$user = new UsuarioController();
