8982526dd5f4834eae6502c6ad1933272e918570
Arthur Tavares <arthurabreu00@gmail.com>
2019-05-24 21:00:30 -0300

    Ajeitando layout da index

--- bug/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    spl_autoload_register(function($class_name){
-        $filename = 'class/'.$class_name.".php";
-        if (file_exists(($filename))) {
-            require_once($filename);
-        }
-    });
- ?>
