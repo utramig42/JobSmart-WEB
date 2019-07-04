66e033e42b028d4cd744b185bf43a5430d5c8cfb
Arthur Tavares <arthurabreu00@gmail.com>
2019-07-04 18:23:02 -0300

    Finalização

--- core/dll/Pagination.php
+++ core/dll/Pagination.php
@@ -1,16 +1,17 @@
  <!-- <?php
-    if (file_exists('../dao/Connection.php')) require_once '../dao/Connection.php';
+        if (file_exists('../dao/Connection.php')) require_once '../dao/Connection.php';
 
-    $Sql = new Connection();
-    // Calculo de número de paginas total.
-    $numTotal =  $Sql->numOfRows("SELECT * FROM $table"); // Número de linhas total no banco.
-    $maxPages = ceil($numTotal / $maxItens); // Número total de paginas retornadas.
+        $Sql = new Connection();
+        // Calculo de número de paginas total.
+        $numTotal =  $Sql->numOfRows("SELECT * FROM $table"); // Número de linhas total no banco.
+        $maxPages = ceil($numTotal / $maxItens); // Número total de paginas retornadas.
 
-    $disablePrev = $page == 0 ? 'disabled' : '';
-    $disableProx = $page == $maxPages - 1 ? 'disabled' : '';
+        $disablePrev = $page == 0 ? 'disabled' : '';
+        $disableProx = $page == $maxPages - 1 ? 'disabled' : '';
 
-    ?>
- <!-- Pagination Buttons -->
+        ?>
+-->
+ <!-- Pagination Buttons 
  <nav aria-label="Paginação de tabelas dos fornecedores">
 
      <ul class="pagination justify-content-center">
@@ -23,15 +24,16 @@
          <?php for ($i = 0; $i < $maxPages; $i++) {
                 $active = $page == $i ?  'active' : ''; ?>
 
-             <li class="page-item <?php echo $active ?>">
-                 <a class="page-link" href="?pagina=<?php echo $i; ?>">
-                     <?php echo $i + 1; ?>
-                 </a>
-             </li>
+                 <li class="page-item <?php echo $active ?>">
+                     <a class="page-link" href="?pagina=<?php echo $i; ?>">
+                         <?php echo $i + 1; ?>
+                     </a>
+                 </li>
 
          <?php } ?>
 
-         <li class="page-item <?php echo $disableProx ?>"><a class="page-link" href="?pagina=<?php echo $page >= $maxPages - 1 ?  $numPage : $page + 1; ?>">Próximo</a>
+         <li class="page-item <?php echo $disableProx ?>"><a class="page-link"
+                 href="?pagina=<?php echo $page >= $maxPages - 1 ?  $numPage : $page + 1; ?>">Próximo</a>
          </li>
      </ul>
  </nav> -->
